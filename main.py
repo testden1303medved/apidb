c3dbbb35b006384722ebfc00630801e0953b80e6
testden1303medved <126058089+testden1303medved@users.noreply.github.com>
2025-01-20 21:44:18 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,31 @@ from flask import Flask, jsonify, request
 import random
 
 app = Flask(__name__)
-giveways = []
+giveaways = []
 
 @app.route("/")
 def index():
     return "Hello, World!"
 
-@app.route('/giveway', methods=['DELETE', 'GET'])
+@app.route('/giveaway', methods=['DELETE', 'GET'])
 def giveaway():
     if request.method == "DELETE":
-        givewayId = request.json.get("id")
+        giveawayId = request.json.get("id")
 
-        if not givewayId:             return jsonify({"message": "Failure | Giveway id required"}), 400
-        if givewayId not in giveways: return jsonify({"message": "Failure | Giveway id not found"}), 400
+        if not giveawayId:             return jsonify({"message": "Failure | giveaway id required"}), 400
+        if giveawayId not in giveaways: return jsonify({"message": "Failure | giveaway id not found"}), 400
 
-        giveways.remove(int(id))
+        giveaways.remove(int(id))
 
         return jsonify({"message": "Success"}), 200
     elif request.method == "GET":
 
-        randomGivewayId = random.randint(0, 999999999)
-        while randomGivewayId not in giveways:
-            randomGivewayId = random.randint(0, 999999999)
+        randomgiveawayId = random.randint(0, 9999999999999999)
         
-        giveways.append(randomGivewayId)
+        giveaways.append(randomgiveawayId)
 
-        return jsonify({"message": f"Success | {randomGivewayId}"}), 200
+        return jsonify({"message": f"Success | {randomgiveawayId}"}), 200
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=8080)
+    app.run(host="0.0.0.0",port=9472)
