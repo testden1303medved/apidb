552c62532d7046ca4d09c8164a03c5e98e28925d
testden1303medved <126058089+testden1303medved@users.noreply.github.com>
2025-01-20 20:52:32 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from flask import Flask, jsonify, request
+import random
 
 app = Flask(__name__)
-rdb  = []
+giveways = []
 
 @app.route("/")
 def index():
     return "Hello, World!"
 
-@app.route('/giveway', methods=['POST', 'GET'])
+@app.route('/giveway', methods=['DELETE', 'GET'])
 def giveaway():
-    rid = request.headers.get("rid")
-    if not rid: return jsonify({"error": "Role Id (rid) is required"}), 400
-    print(f"{rid} /// {request.method}")
-    if request.method == "POST":
-        if rid in rdb: return jsonify({"message": ""}), 201
-        rdb.append(rid)
-        return jsonify({"message": ""}), 200
+    if request.method == "DELETE":
+        givewayId = request.json.get("id")
+
+        if not givewayId:             return jsonify({"message": "Failure | Giveway id required"}), 400
+        if givewayId not in giveways: return jsonify({"message": "Failure | Giveway id not found"}), 400
+
+        giveways.remove(int(id))
+
+        return jsonify({"message": "Success"}), 200
     elif request.method == "GET":
-        if rid in rdb: 
-            rdb.remove(rid)
-            return jsonify({"message": ""}), 200
-        else: return jsonify({"message": ""}), 400
+
+        randomGivewayId = random.randint(0, 999999999)
+        while randomGivewayId not in giveways:
+            randomGivewayId = random.randint(0, 999999999)
+        
+        giveways.append(randomGivewayId)
+
+        return jsonify({"message": f"Success | {randomGivewayId}"}), 200
 
 
 if __name__ == "__main__":
